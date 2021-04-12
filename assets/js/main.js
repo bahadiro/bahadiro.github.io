db3b81de85fee63a360fb0185c4938c386ac5842
bahadiro <ozdemirbahadir@gmail.com>
2021-04-11 21:29:06 -0700

    Initial Commit

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+(function(document) {
+  var toggle = document.querySelector('.sidebar-toggle');
+  var sidebar = document.querySelector('#sidebar');
+  var checkbox = document.querySelector('#sidebar-checkbox');
+
+  document.addEventListener('click', function(e) {
+    var target = e.target;
+
+    if(!checkbox.checked ||
+       sidebar.contains(target) ||
+       (target === checkbox || target === toggle)) return;
+
+    checkbox.checked = false;
+  }, false);
+
+  // Updates the progress bar.
+  document.addEventListener('scroll', function() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+
+    if(document.getElementById("progressBar") == null) return;
+
+    document.getElementById("progressBar").style.width = scrolled + "%";
+  });
+})(document);
